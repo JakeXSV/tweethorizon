e30924a1865fd689510d2f8200412e74f38f8d02
Jake Prather <JakeHP@Zoho.com>
2015-03-07 09:34:32 -0600

    Added js uglify. Simplified lint paths.

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,12 @@
 var gulp = require('gulp');
 var jshint = require('gulp-jshint');
+var uglify = require('gulp-uglify');
 
 gulp.task('lint', function() {
     return gulp.src(
         [
             /* Client */
-            './public/js/*.js',
-            './public/js/store/*.js',
-            './public/js/store/models/*.js',
-            './public/js/view/*.js',
-            './public/js/util/*.js',
+            './public/js/**/*.js',
             /* Server */
             './util/*.js',
             './twitter/*.js',
@@ -21,4 +18,10 @@ gulp.task('lint', function() {
         .on('error', function (error) {
             console.error(String(error));
         });
+});
+
+gulp.task('minify-js', function() {
+    gulp.src('./public/js/**/*.js', {base: "./public"})
+        .pipe(uglify())
+        .pipe(gulp.dest('./public/dist'))
 });
